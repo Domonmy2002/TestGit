3764d6bf85b710681824e8128c075d116ef23b18
Domonmy2002 <Domonmy2002@126.com>
2012-07-19 20:02:55 +0800

    增加异常处理

--- child_process/exec.js
+++ child_process/exec.js
@@ -2,6 +2,7 @@ var exec = require("child_process").exec;
 //pngÍ¼Æ¬Ñ¹Ëõ
 process.nextTick(function(){
 	var args = process.argv.slice(2);
+	if(args.length < 2) throw Error("Invalid parameters..");
 	exec("E:/software/pngcrush-1.7.15/pngcrush.exe -rem alla -brute " + args[0] + " " + args[1], function(error, stdout, stderr){
 		console.log("stdout:" + stdout);
 		console.log("stderr:" + stderr);
